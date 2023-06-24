b96c7884af9eab33e7c00c1402b42f8f5af8af8f
BuildTools <unconfigured@null.spigotmc.org>
2023-06-24 05:11:56 -0500

    Initial commit

---
+++ public/js/editor/objects/circle.js
@@ -0,0 +1,46 @@
+﻿
+/* box.js */
+
+import { Object, toRadians } from './object.js'
+
+export class Circle extends Object {
+    constructor(properties) {
+        super(properties)
+    }
+
+    draw(drawer) {
+        const x = this.getX()
+        const y = this.getY()
+        const width = this.getWidth()
+        const height = this.getHeight()
+        const rotation = this.getRotation()
+
+        const needsRotation = (this.getWidth() != this.getHeight() || rotation % 90 != 0) // if its a cube and its rotation is 0, 90, 180, or 270, then ignore it!
+        drawer.fillStyle = this.getColor()
+        
+        if (needsRotation) {
+            drawer.translate(x + width / 2, y + height / 2) // move pivot to center of the shape
+            drawer.rotate(-toRadians(rotation)) // rotate
+            drawer.translate(-(x + width / 2), -(y + height / 2)) // move the pivot back
+        }
+
+        drawer.fillRect(x, y, width, height) // draw the rectangle
+
+        if (needsRotation)
+            drawer.resetTransform() // reset transform for other shapes
+    }
+
+    inBounds(boundX, boundY) { // TODO: rotation, only "detects" default rotation of zero
+        const x = this.getX()
+        const y = this.getY()
+
+        return  boundX >= x &&
+                boundX <= x + this.getWidth() &&
+                boundY >= y &&
+                boundY <= y + this.getHeight()
+    }
+}
+
+export function create(properties) {
+    return new Circle(properties)
+}
---
+++ public/js/editor/objects/size_handle.js
@@ -0,0 +1,18 @@
+﻿
+/* size_handle.js */
+
+import { Handle } from './handle.js'
+
+export class SizeHandle extends Handle {
+    constructor(properties) {
+        super(properties)
+    }
+
+    getRotation() {
+        return 0
+    }
+}
+
+export function create(properties) {
+    return new SizeHandle(properties)
+}
